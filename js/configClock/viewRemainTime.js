0ef426c6234fddbaf113cc26dc0af12df7f79fe5
Trần Duy Bá <tranduyba2599@gmail.com>
2020-12-31 21:56:33 +0700

    Cập nhật thời gian tết nguyên đán 2021

--- js/configClock/viewRemainTime.js
+++ js/configClock/viewRemainTime.js
@@ -3,7 +3,7 @@
 /*----------------------danh sách số từ 0-9 cho từng view phần tử thời gian sao cho đúng nhất------*/
 /**************************************************************************************************/
 var viewRemainTime = function(){
-    this.objRemainTime = new remainTime(2020, 1, 25, 00, 00, 00, 000); // Lấy ra mảng chứa các thành phần time còn lại
+    this.objRemainTime = new remainTime(2021, 2, 12, 00, 00, 00, 000); // Lấy ra mảng chứa các thành phần time còn lại
     this.remainTime = []; // Chứa mảng các thành phần thời gian trong thời gian còn lại
     this.listNumberOfViewElementTime = []; // Mảng các danh sách số từ 0-9 trong các view hiển thị phần tử thời gian
     this.viewElementTime = document.getElementsByClassName("element-time"); // Mảng chứa các view phần từ thời gian
